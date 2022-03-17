28c5324a524ccd136e42fb9446e6cfcd67172779
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-03-17 14:49:42 -0600

    TM-13 #inprogress #comment part of connection

---
+++ App/config/dependencies.js
@@ -0,0 +1,10 @@
+// General
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require('dotenv');
+
+module.exports = {
+    express, morgan, mongoose, path, dotenv
+}
---
+++ App/config/properties.js
@@ -0,0 +1,8 @@
+const { dotenv } = require('./dependencies');
+dotenv.config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET
+}
