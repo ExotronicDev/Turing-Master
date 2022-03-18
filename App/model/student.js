eaccecdbc3921f48b580d470d162ee83b7a16a58
Exotronic <ecotro2014gd@gmail.com>
2022-03-17 21:12:19 -0600

    TM-10 #inprogress #comment attributes set for student

---
+++ App/model/student.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const Student = mongoose.model("Student", new Schema({
+    id: {type: Number, index: true, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true, minlength: 8},
+    tMachines: [{
+        id: {type: Number, required: true},
+        description: {type: String}
+    }]
+}));
