089a160ebd2725ccf0be1e824d65662f31c9e0ba
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-03-21 16:52:52 -0600

    Added colors to package

--- App/control/controllers/Controller.js
+++ App/control/controllers/Controller.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("../../middleware/async");
+const ErrorResponse = require("./../../utils/errorResponse");
 const StudentController = require("./StudentController");
 
 //  @desc       Get all Students
