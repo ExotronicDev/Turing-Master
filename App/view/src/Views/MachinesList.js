38f61d0bf8c0a948f8da2dfa3afe84ef2b6681a9
Mario Andrés Camacho Guzmán <71361551+jumpman2301@users.noreply.github.com>
2022-03-23 21:55:27 -0600

    Add
    MachineLists.js added

---
+++ App/view/src/Views/MachinesList.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+	render() {
+		return (
+			<div className="TMList">
+				<p id="title">User info</p>
+				<form id="form">
+					<div class="form-group">
+						<label for="email">Email</label>
+						<div className="form-group">
+							<input
+								type="email"
+								className="form-control"
+								id="email"
+								placeholder="email@example.com"
+								name="email"
+							/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<div className="form-group">
+							<input
+								type="password"
+								className="form-control"
+								id="password"
+								name="password"
+							/>
+						</div>
+					</div>
+					<button
+						type="submit"
+						className="btn btn-primary"
+						style={{ marginTop: "20px", width: "200px" }}
+					>
+						Login
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Login;
