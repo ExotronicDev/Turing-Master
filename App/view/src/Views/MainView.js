a71c0c2cec4612541ed87e3c56e88fbc42620c71
Chuz23 <48294718+Chuz23@users.noreply.github.com>
2022-03-25 00:21:22 -0600

    TM-44 #inprogress

---
+++ App/view/src/Views/MainView.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class MainView extends Component {
+	render() {
+		return (
+			<div className="MainView">
+				<p>Turing Machine Simulator</p>
+				<form action="">
+					<button
+						id="newmachine"
+						type="submit"
+						className="btn btn-primary btn-lg"
+					>
+						{" "}
+						New Machine{" "}
+					</button>
+				</form>
+				<form action="">
+					<button
+						id="mymachines"
+						type="submit"
+						className="btn btn-primary btn-lg"
+					>
+						{" "}
+						View My Machines{" "}
+					</button>
+				</form>
+				<form action="">
+					<button
+						id="edit"
+						type="submit"
+						className="btn btn-primary btn-lg"
+					>
+						{" "}
+						Edit my Profile{" "}
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default MainView;
