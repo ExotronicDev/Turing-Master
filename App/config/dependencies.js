dabb983862c2f0c6d7812b7a18db7e4c533f965c
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-03-28 08:42:28 -0600

    Colors sirve

--- App/config/dependencies.js
+++ App/config/dependencies.js
@@ -3,10 +3,13 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const dotenv = require('dotenv');
-//const colors = require("colors");
+const dotenv = require("dotenv");
+const colors = require("colors");
 
 module.exports = {
-    express, morgan, path, 
-    dotenv, mongoose
-}
+	express,
+	morgan,
+	path,
+	dotenv,
+	mongoose,
+};
