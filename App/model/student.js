8f6351d27911dd864190e93a037ff566afc7da53
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-03-29 17:50:36 -0600

    Model fields refined

--- App/model/student.js
+++ App/model/student.js
@@ -1,16 +1,41 @@
 const { mongoose } = require("../config/dependencies");
 const { Schema } = mongoose;
 
-const Student = mongoose.model("Student", new Schema({
-    id: {type: Number, index: true, unique: true, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true, minlength: 8},
-    tMachines: [{
-        id: {type: Number, required: true},
-        description: {type: String}
-    }]
-}));
+const Student = mongoose.model(
+	"Student",
+	new Schema({
+		id: {
+			type: Number,
+			index: true,
+			unique: true,
+			required: [true, "Please add an id."],
+		},
+		firstName: {
+			type: String,
+			required: [true, "Please add a first name."],
+		},
+		lastName: { type: String, required: [true, "Please add a last name."] },
+		email: {
+			type: String,
+			required: [true, "Please add an email."],
+			match: [
+				/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+				"Please add a valid email.",
+			],
+		},
+		password: {
+			type: String,
+			required: [true, "Please add a password."],
+			minlength: 8,
+			select: false,
+		},
+		tMachines: [
+			{
+				id: { type: Number, required: true },
+				description: { type: String },
+			},
+		],
+	})
+);
 
 module.exports = Student;
--- App/model/tmachine.js
+++ App/model/tmachine.js
@@ -1,23 +1,33 @@
 const { mongoose } = require("../config/dependencies");
 const { Schema } = mongoose;
 
-const TMachine = mongoose.model("TMachine", new Schema({
-    id: {type: Number, index: true, unique: true, required: true},
-    description: {type: String, required: true},
-    owner: {
-        id: {type: Number, required: true}
-    },
-    initialState: {
-        id: {type: Number},
-        name: {type: String}
-    },
-    collaborators: [{
-        id: {type: Number}
-    }],
-    states: [{
-        id: {type: Number},
-        name: {type: String}
-    }]
-}));
+const TMachine = mongoose.model(
+	"TMachine",
+	new Schema({
+		id: { type: Number, index: true, unique: true, required: true },
+		description: {
+			type: String,
+			required: [true, "Please add a description to the Turing Machine."],
+		},
+		owner: {
+			id: { type: Number, required: true },
+		},
+		initialState: {
+			id: { type: Number },
+			name: { type: String },
+		},
+		collaborators: [
+			{
+				id: { type: Number },
+			},
+		],
+		states: [
+			{
+				id: { type: Number },
+				name: { type: String },
+			},
+		],
+	})
+);
 
 module.exports = TMachine;
