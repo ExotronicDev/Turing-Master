ae6b71761c858bdd3ce17220e4c7dd833d1e186e
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-04-01 01:32:58 -0600

    TEST heroku port workaround

--- App/server.js
+++ App/server.js
@@ -41,7 +41,8 @@ app.use(errorHandler);
 
 // Start server
 const server = app.listen(
-	process.env.PORT || 8080,
+	// process.env.PORT || 8080,
+	8080,
 	console.log(`Server initialized on port: ${process.env.PORT}.`.yellow.bold)
 );
 
