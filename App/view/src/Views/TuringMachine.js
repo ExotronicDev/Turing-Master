036c1e95451c378fa00c7be51b219aab22b76ddc
Mario Andrés Camacho Guzmán <71361551+jumpman2301@users.noreply.github.com>
2022-04-01 03:23:01 -0600

    Changes

--- App/view/src/Views/TuringMachine.js
+++ App/view/src/Views/TuringMachine.js
@@ -1,74 +1,279 @@
-'use strict';
-/**
- * Construct a Turing machine.
- * @param {(state, symbol) -> ?{state: state, symbol: symbol, move: direction}}
- *   transition
- *   A transition function that, given *only* the current state and symbol,
- *   returns an object with the following properties: symbol, move, and state.
- *   Returning null/undefined halts the machine (no transition defined).
- * @param {state} startState  The state to start in.
- * @param         tape        The tape to use.
- */
-function TuringMachine(transition, startState, tape) {
-  this.transition = transition;
-  this.state = startState;
-  this.tape = tape;
-}
+// important variables for a web based implementation
+var tapelength = 5;
+var leftmost = 1000;
 
-TuringMachine.prototype.toString = function () {
-  return String(this.state) + '\n' + String(this.tape);
-};
+// read the user's rules
+var turingMachineRules = [];
 
-/**
- * Step to the next configuration according to the transition function.
- * @return {boolean} true if successful (the transition is defined),
- *   false otherwise (machine halted)
- */
-TuringMachine.prototype.step = function () {
-  var instruct = this.nextInstruction;
-  if (instruct == null) { return false; }
+// TM parts tracker
+var currentState = "I";
+var accept = false;
+var halt = false;
 
-  this.tape.write(instruct.symbol);
-  move(this.tape, instruct.move);
-  this.state = instruct.state;
+var fuelLevel = 0;
+var autoSpeed = 100;
+var autoRunning = false;
 
-  return true;
-};
-
-Object.defineProperties(TuringMachine.prototype, {
-  nextInstruction: {
-    get: function () { return this.transition(this.state, this.tape.read()); },
-    enumerable: true
-  },
-  isHalted: {
-    get: function () { return this.nextInstruction == null; },
-    enumerable: true
-  }
-});
-
-// Allows for both notational conventions of moving the head or moving the tape
-function move(tape, direction) {
-  switch (direction) {
-    case MoveHead.right: tape.headRight(); break;
-    case MoveHead.left:  tape.headLeft();  break;
-    default: throw new TypeError('not a valid tape movement: ' + String(direction));
+// move the current head to the left and prepend cells if necessary
+function moveL() {
+  var element = document.getElementsByClassName("head")[0];
+  if (Number(element.id) - 1 >= leftmost) {
+    element.classList.remove("head");
+    var next = Number(element.id) - 1;
+    var nextel = document.getElementById(next);
+    nextel.classList.add("head");
+  } else {
+    // add 4 cells to the beginning
+    prependCell();
+    prependCell();
+    prependCell();
+    prependCell();
+    moveL();
   }
 }
-var MoveHead = Object.freeze({
-  left:  {toString: function () { return 'L'; } },
-  right: {toString: function () { return 'R'; } }
-});
-var MoveTape = Object.freeze({left: MoveHead.right, right: MoveHead.left});
 
-exports.MoveHead = MoveHead;
-exports.MoveTape = MoveTape;
-exports.TuringMachine = TuringMachine;
+// move the current head to the right and append cells if necessary
+function moveR() {
+  var element = document.getElementsByClassName("head")[0];
+  if (Number(element.id) + 1 < leftmost + tapelength) {
+    element.classList.remove("head");
+    var next = Number(element.id) + 1;
+    var nextel = document.getElementById(next);
+    nextel.classList.add("head");
+  } else {
+    // add 4 cells to the end
+    appendCell();
+    appendCell();
+    appendCell();
+    appendCell();
+    moveR();
+  }
+}
 
+function move(direction) {
+  if (direction == "L") moveL();
+  if (direction == "R") moveR();
+}
 
+// write a value to the current head
+function tmWriter(ele) {
+  var head = document.getElementsByClassName("head")[0];
+  head.innerHTML = ele;
+}
 
-/*****************
- ** WEBPACK FOOTER
- ** ./src/TuringMachine.js
- ** module id = 11
- ** module chunks = 0
- **/
+// get the value from the current head
+function tmReader() {
+  var head = document.getElementsByClassName("head")[0];
+  return head.innerHTML;
+}
+
+// add a cell at the left end of the machine
+function prependCell() {
+  var machine = document.getElementById("machine");
+  var baby = document.createElement("DIV");
+  baby.id = String(leftmost - 1);
+  baby.classList.add("cell");
+  machine.prepend(baby);
+  leftmost -= 1;
+  tapelength += 1;
+}
+
+// add a cell at the right end of the machine
+function appendCell() {
+  var machine = document.getElementById("machine");
+  var baby = document.createElement("DIV");
+  baby.id = String(leftmost + tapelength);
+  baby.classList.add("cell");
+  machine.append(baby);
+  tapelength += 1;
+}
+
+function updateState() {
+  document.getElementById("state").innerHTML = currentState;
+}
+
+function setup() {
+  cleartm();
+  // get starting string
+  // put it in the tape
+  var startingString = document.getElementById("startString").value;
+  for (var i = 0; i < startingString.length; i++) {if (window.CP.shouldStopExecution(0)) break;
+    tmWriter(startingString[i]);
+    moveR();
+  }
+  // set the head back to leftmost
+  window.CP.exitedLoop(0);var element = document.getElementsByClassName("head")[0];
+  element.classList.remove("head");
+  var nextel = document.getElementById(leftmost);
+  nextel.classList.add("head");
+
+  // get user's TM description
+  var markdown = document.getElementById("markdown").value.toUpperCase();
+  markdown = markdown.replace(/\n\s*\n/g, "\n"); // ignore duplicate newlines
+  var rules = markdown.split("\n");
+  for (var i = 0; i < rules.length; i++) {if (window.CP.shouldStopExecution(1)) break;
+    parseRule(rules[i]);
+  }
+
+  // set current state
+  window.CP.exitedLoop(1);document.getElementById("state").innerHTML = currentState;
+
+  // prependCell();
+  moveR();
+}
+
+function findComment(element) {
+  return element == "!";
+}
+
+// add each rule to the list of rules
+function parseRule(e) {
+  if (e.charAt(0) != "!") {
+    // ignore comments
+    e = e.replace(/ +(?= )/g, ""); // ignore duplicate spaces
+    var parts = e.split(" ");
+    var commentPosition = parts.findIndex(findComment); // finding more comments
+    if (commentPosition >= 0) {
+      parts.length = commentPosition;
+    }
+    turingMachineRules.push(parts);
+  }
+}
+
+function cleartm() {
+  // document.location.reload(true);
+  // document.getElementById("markdown").value = "";
+  // document.getElementById("startString").value = "";
+  document.getElementById("state").innerHTML = "";
+  document.getElementById("machine").innerHTML = "";
+
+  leftmost = 1000;
+  turingMachineRules = [];
+  currentState = "I";
+  accept = false;
+  halt = false;
+
+  var baby = document.createElement("DIV");
+  baby.id = String(leftmost);
+  baby.classList.add("cell");
+  baby.classList.add("head");
+
+  document.getElementById("machine").append(baby);
+  tapelength = 1;
+
+  appendCell();
+  appendCell();
+  appendCell();
+  prependCell();
+}
+
+function same(ele, read) {
+  if (ele == "_" && read == "") {
+    return true;
+  } else if (ele == read) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function step() {
+  if (!halt) {
+    var notfound = true;
+    for (var i = 0; i < turingMachineRules.length; i++) {if (window.CP.shouldStopExecution(2)) break;
+      var element = turingMachineRules[i];
+      if (element[0] == currentState && same(element[1], tmReader())) {
+        currentState = element[2];
+        tmWriter(element[3]);
+        move(element[4]);
+        notfound = false;
+        break;
+      }
+    }window.CP.exitedLoop(2);
+
+    if (notfound) {
+      halt = true;
+    }
+
+    if (halt) {
+      if (currentState == "ACCEPT") {
+        accept = true;
+        document.getElementById("state").innerHTML = "ACC";
+      } else {
+        accept = false;
+        document.getElementById("state").innerHTML = "HALT";
+      }
+    }
+    updateState();
+  } // if you've halted then clicking "step" does nothing.
+}
+
+function run() {
+  // set "fuel"
+  fuelLevel = document.getElementById("fuelLevel").value;
+  autoSpeed = document.getElementById("autoSpeed").value;
+
+  if (!autoRunning) {
+    autoRunning = true;
+    setup();
+    setTimeout(function () {
+      stepWithFuel();
+    }, 200);
+  }
+}
+
+function stepWithFuel() {
+  if (fuelLevel > 0 && !halt) {
+    document.getElementById("stepbutton").click();
+    fuelLevel -= 1;
+    document.getElementById("remainingFuel").innerHTML = fuelLevel;
+    setTimeout(function () {
+      stepWithFuel();
+    }, autoSpeed);
+  } else if (!halt) {
+    halt = true;
+    accept = false;
+    document.getElementById("state").innerHTML = "NO FUEL";
+    autoRunning = false;
+  } else {
+    // done
+    autoRunning = false;
+  }
+}
+
+/*
+TMState:
+Head:
+Tape:
+
+Syntax
+Current State, Data on tape, Data to write, Move, Next State
+use a dictionary based on current state?
+currentstate read nextstate write move
+<q, s, q', s', r>
+crnwm
+*/
+
+function addMachine1() {
+  var markdownBox = document.getElementById("markdown");
+  var startStringBox = document.getElementById("startString");
+  markdownBox.value =
+  "! Slightly Busy Beaver\n\ni 0 i 0 r\ni 1 back 0 l\n\nback 0 back 1 l\nback _ i _ r\n\ni _ accept _ s";
+  startStringBox.value = "000001";
+}
+
+function addMachine2() {
+  var markdownBox = document.getElementById("markdown");
+  var startStringBox = document.getElementById("startString");
+  markdownBox.value =
+  "! Type in your Turing Machine Markdown here:\n! CurrentState Read NextState Write Move\n! EXAMPLE 1\n! starting string: 0101010101\n! this is a comment\ni 0 i 0 r\ni 1 q2 0 l\nq2 0 q2 0 l ! you can write whatever you want here\nq2 1 i 0 r\n\n! final states\nq1 _ accept  _ s\nq2 _ accept  _ s ! with a comment in the line";
+  startStringBox.value = "0101010101";
+}
+
+function addMachine3() {
+  var markdownBox = document.getElementById("markdown");
+  var startStringBox = document.getElementById("startString");
+  markdownBox.value =
+  "! EXAMPLE 2\n! starting string: 00001\ni 0 i 1 r\ni 1 back 0 l\n\nback 1 back 1 l\nback _ i _ r\n\ni _ accept _ s";
+  startStringBox.value = "00001";
+}
