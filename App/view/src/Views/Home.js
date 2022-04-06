f7c90f87155682247d7e46bcf594b3425d0c6f5c
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2022-04-05 22:03:24 -0600

    Home retocado

--- App/view/src/Views/Home.js
+++ App/view/src/Views/Home.js
@@ -4,37 +4,31 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="Home">
-				<p className="titulo">Turing Master</p>
-				<form action="/login">
-					<button
-						id="login"
-						type="submit"
-						className="btn btn-primary btn-lg"
-					>
-						{" "}
-						Login{" "}
-					</button>
-				</form>
-				<form action="/register">
-					<button
-						id="register"
-						type="submit"
-						className="btn btn-primary btn-lg"
-					>
-						{" "}
-						Register{" "}
-					</button>
-				</form>
-				<form action="/TuringMachine">
-					<button
-						id="TMachine"
-						type="submit"
-						className="btn btn-primary btn-lg"
-					>
-						{" "}
-						Simulator{" "}
-					</button>
-				</form>
+				<div id="container">
+					<p className="title">Turing Master</p>
+					<div id="options">
+						<form action="/login">
+							<button
+								id="login"
+								type="submit"
+								className="btn btn-primary btn-lg"
+							>
+								{" "}
+								Login{" "}
+							</button>
+						</form>
+						<form action="/register">
+							<button
+								id="register"
+								type="submit"
+								className="btn btn-primary btn-lg"
+							>
+								{" "}
+								Register{" "}
+							</button>
+						</form>
+					</div>
+				</div>
 			</div>
 		);
 	}
